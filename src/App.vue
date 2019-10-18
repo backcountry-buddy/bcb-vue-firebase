4ea2ef531d04d4a5ddb7496a9a3332ec1ee1c751
Christian Spanring <cspanring@gmail.com>
2019-10-17 21:01:39 -0400

    adds tagline

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
-  <div id="app" class="font-sans p-4 lg:mx-auto lg:w-1/2">
+  <div id="app" class="font-sans px-2 md:p-4 lg:mx-auto lg:w-1/2">
     <div class="flex justify-between items-center py-2">
-      <h1 class="flex-grow text-xl font-semibold whitespace-no-wrap">
-        <router-link to="/">Backcountry Buddy</router-link>
+      <h1 class="flex flex-col flex-grow">
+        <router-link
+          to="/"
+          class="text-xl font-semibold whitespace-no-wrap underline"
+          >Backcountry Buddy</router-link
+        >
+        <span class="text-sm font-light italic"
+          >Find your next tour buddies!</span
+        >
       </h1>
       <LoginForm />
     </div>

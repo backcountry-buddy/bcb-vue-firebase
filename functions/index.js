ccea00e08fb7a56ba30fd86a4e8adebf8124ca6a
Christian Spanring <cspanring@gmail.com>
2019-10-26 19:04:30 -0400

    create default user profile on signup resolves #42

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,26 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 admin.initializeApp();
 
 const db = admin.firestore();
 
 // create parent docs, if they don't exist
 exports.createLocationParents = functions.firestore
-  .document("locations/{locationId}")
+  .document('locations/{locationId}')
   .onCreate((snap, context) => {
     const location = snap.data();
     let parents = [
       {
-        prop: "country",
-        coll: "countries"
+        prop: 'country',
+        coll: 'countries'
       },
       {
-        prop: "state",
-        coll: "states"
+        prop: 'state',
+        coll: 'states'
       },
       {
-        prop: "region",
-        coll: "regions"
+        prop: 'region',
+        coll: 'regions'
       }
     ];
     const queries = parents.map(parent => {
@@ -28,7 +28,7 @@ exports.createLocationParents = functions.firestore
       if (!location[prop]) {
         return Promise.resolve();
       }
-      const query = db.collection(coll).where("name", "==", location[prop]);
+      const query = db.collection(coll).where('name', '==', location[prop]);
       return query
         .get()
         .then(querySnapshot => {
@@ -36,10 +36,10 @@ exports.createLocationParents = functions.firestore
             const payload = {
               name: location[prop]
             };
-            if (["state", "region"].includes(prop)) {
+            if (['state', 'region'].includes(prop)) {
               payload.country = location.country;
             }
-            if (prop === "region") {
+            if (prop === 'region') {
               payload.state = location.state;
             }
             return db.collection(coll).add(payload);
@@ -55,22 +55,34 @@ exports.createLocationParents = functions.firestore
 
 // Buddy counts
 exports.incrementBuddyCount = functions.firestore
-  .document("tours/{tourId}/buddies/{buddyId}")
+  .document('tours/{tourId}/buddies/{buddyId}')
   .onCreate((snap, context) => {
     const tourId = context.params.tourId;
     const nrBuddies = admin.firestore.FieldValue.increment(1);
     return db
-      .collection("tours")
+      .collection('tours')
       .doc(tourId)
       .update({ nrBuddies });
   });
 exports.decrementBuddyCount = functions.firestore
-  .document("tours/{tourId}/buddies/{buddyId}")
+  .document('tours/{tourId}/buddies/{buddyId}')
   .onDelete((snap, context) => {
     const tourId = context.params.tourId;
     const nrBuddies = admin.firestore.FieldValue.increment(-1);
     return db
-      .collection("tours")
+      .collection('tours')
       .doc(tourId)
       .update({ nrBuddies });
   });
+
+// create user profile with a default displayName on user signup
+exports.createUserProfile = functions.auth.user().onCreate(user => {
+  const { uid, email } = user;
+  const profile = {
+    displayName: `${email.split('@')[0]}`
+  };
+  return db
+    .collection('users')
+    .doc(uid)
+    .set(profile);
+});
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="profileIsLoaded" class="mt-2 mb-4">
+  <div class="mt-2 mb-4">
     <UserProfileForm
       v-if="isEditing"
       v-on:toggle-editing="toggleEditing"
@@ -7,27 +7,18 @@
       :currentUser="currentUser"
     />
     <div v-else class="mt-2 mb-4">
-      <div v-if="hasProfile">
-        <UserProfile :profile="profile" :currentUser="currentUser"
-          ><router-link
-            class="link-xs mr-2"
-            :to="{ name: 'userDetail', params: { uid: currentUser.uid } }"
-          >
-            Public profile</router-link
-          >
-          <button type="button" class="form-button" @click="toggleEditing">
-            Edit Profile
-          </button></UserProfile
+      <UserProfile :profile="profile" :currentUser="currentUser"
+        ><router-link
+          class="link-xs mr-2"
+          :to="{ name: 'userDetail', params: { uid: currentUser.uid } }"
         >
-      </div>
-      <div v-else class="text-center">
-        <p class="mb-2">You don't have a public profile.</p>
-        <button type="button" class="form-button" @click="createProfile">
-          Create one now!
-        </button>
-      </div>
+          Public profile</router-link
+        >
+        <button type="button" class="form-button" @click="toggleEditing">
+          Edit Profile
+        </button></UserProfile
+      >
     </div>
-    <ErrorMessage :errorMessage="errorMessage" />
   </div>
 </template>
 
@@ -35,36 +26,22 @@
 import { auth, db } from '@/config/firebase';
 import UserProfile from '@/components/UserProfile.vue';
 import UserProfileForm from '@/components/UserProfileForm.vue';
-import ErrorMessage from '@/components/ErrorMessage.vue';
 
 export default {
   data() {
     return {
-      profileIsLoaded: false,
-      hasProfile: false,
       isEditing: false,
       currentUser: {
         uid: '',
         email: '',
         displayName: ''
       },
-      profile: {
-        displayName: '',
-        homeLocation: '',
-        travelDestinations: '',
-        experienceLocations: '',
-        experienceLevel: '',
-        aiare: '',
-        pack: '',
-        otherInfo: ''
-      },
-      errorMessage: ''
+      profile: {}
     };
   },
   components: {
     UserProfile,
-    UserProfileForm,
-    ErrorMessage
+    UserProfileForm
   },
   created() {
     const currentUser = auth.currentUser;
@@ -79,37 +56,14 @@ export default {
     toggleEditing() {
       this.isEditing = !this.isEditing;
     },
-    fetchUserProfile(uid) {
-      this.errorMessage = '';
-      db.collection('users')
+    async fetchUserProfile(uid) {
+      const doc = await db
+        .collection('users')
         .doc(uid)
-        .get()
-        .then(doc => {
-          if (doc.exists) {
-            const userProfile = doc.data();
-            this.profile = userProfile;
-            this.hasProfile = true;
-          }
-        })
-        .catch(error => {
-          this.errorMessage = error.message;
-        })
-        .finally(() => {
-          this.profileIsLoaded = true;
-        });
-    },
-    createProfile() {
-      const { uid } = this.currentUser;
-      db.collection('users')
-        .doc(uid)
-        .set(this.profile)
-        .then(() => {
-          this.isEditing = true;
-          this.hasProfile = true;
-        })
-        .catch(error => {
-          this.authErrorMessage = error.message;
-        });
+        .get();
+      if (doc.exists) {
+        this.profile = doc.data();
+      }
     }
   }
 };

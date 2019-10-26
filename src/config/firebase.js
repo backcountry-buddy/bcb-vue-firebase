9f141dfff55e2f784150ad156479c1cac23783be
Christian Spanring <cspanring@gmail.com>
2019-10-26 11:26:17 -0400

    Revert "adds analytics configuration"
    This reverts commit cba5e096806c708ee2ae04764938d4eccce598bb.
    This change doubles (!!) the vendor-chunk size. Analytics are not that important at this point.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,11 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import 'firebase/analytics';
 
 firebase.initializeApp({
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  measurementId: process.env.VUE_APP_MEASUREMENT_ID
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID
 });
 
 export const db = firebase.firestore();
